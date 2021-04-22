33581d8abee36b7e846d9b58a26dca2c123debb7
Nikolai Golub <nikolay.v.golub@gmail.com>
2021-04-22 21:22:36 +0200

    Bring back blockchain-utils

--- src/js/truffle-contract.js
+++ src/js/truffle-contract.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
 let ethJSABI = require("ethjs-abi");
-let BlockchainUtils = require("@truffle/blockchain-utils");
+let BlockchainUtils = require("truffle-blockchain-utils");
 let Web3 = require("web3");
 
 // For browserified version. If browserify gave us an empty version,
